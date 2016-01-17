01ae7a9b5286768bb1bf296a5f6db826b3cfd566
Domicio Jose <testedomicio@hotmail.com>
2016-01-17 14:07:16 -0200

    Eventos - 4

--- Desenrolando_jogos.py
+++ Desenrolando_jogos.py
@@ -1,16 +1,19 @@
 import pygame
-pygame.init()
+blanco = (255, 255, 255)
+negro = (0, 0, 0)
+rojo = (255, 0, 0)
 
-superfice = pygame.display.set_mode((800,600))
+pygame.init()
+superficie = pygame.display.set_mode((800,600))
 pygame.display.set_caption('deslizante')
 
+superficie.fill(blanco)
 pygame.display.update()
 gameExit = False
 while not gameExit:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             gameExit = True
-        
 
     
 pygame.quit()
