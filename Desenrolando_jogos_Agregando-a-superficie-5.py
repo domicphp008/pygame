f550ff4845d8819438e5ee920d0b00ae77fb998f
Domicio Jose <testedomicio@hotmail.com>
2016-01-17 14:24:07 -0200

    pygame

---
+++ Desenrolando_jogos_Agregando-a-superficie-5.py
@@ -0,0 +1,25 @@
+import pygame
+blanco = (255, 255, 255)
+negro = (0, 0, 0)
+rojo = (255, 0, 0)
+
+pygame.init()
+superficie = pygame.display.set_mode((800,600))
+pygame.display.set_caption('deslizante')
+
+
+gameExit = False
+while not gameExit:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            gameExit = True
+    superficie.fill(blanco)
+    pygame.draw.rect(superficie, negro, [400, 300, 10,100])
+    pygame.draw.rect(superficie, rojo,  [400, 300, 10,10])
+
+    
+    pygame.display.update()
+
+    
+pygame.quit()
+quit()
